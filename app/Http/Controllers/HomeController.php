3ec10e806a0677ec0a2d45ca3134ab598266feb9
Antonio Gervacio <luis.gervaciot@gmail.com>
2018-01-12 17:27:53 -0600

    Test 1727

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,9 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function antonioPrueba()
+    {
+        return view('home', ['variable1'=>'Gervacio']);
+    }
 }
