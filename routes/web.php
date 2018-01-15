d4036dbfee2d17c662797fa02839a880505d1d6f
Antonio Gervacio <luis.gervaciot@gmail.com>
2018-01-15 11:12:43 -0600

    1st change over menus data table

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/prueba', 'HomeController@antonioPrueba')->name('antonioPrueba');
+
+Route::get('/menus', function (){
+	return view('menus');
+});
