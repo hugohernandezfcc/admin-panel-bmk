8772b72da179f006f9429559736498fe7070dbc8
Antonio Gervacio <luis.gervaciot@gmail.com>
2018-01-15 18:01:56 -0600

    Test 1758

--- resources/views/menus.blade.php
+++ resources/views/menus.blade.php
@@ -44,15 +44,8 @@
                 <tbody>
                     @foreach ($menus as $menu)
                         <tr>
-                            <td>{{ $menu['text']}}</td>
-                            <td>{{ $menu['order']}}</td>
-                            <td>{{ $menu['label']}}</td>
-                            <td>{{ $menu['icon']}}</td>
-                            <td>{{ $menu['label_color']}}</td>
-                            <td>{{ $menu['url']}}</td>
-                            <td>{{ $menu['to']}}</td>
-                            <td>{{ $menu['typeitem']}}</td>
-                            <td>{{ $menu['parent']}}</td>
+                            <td>{{ $menu -> text]}}</td>
+                            
                         </tr>
                     @endforeach
                 <tfoot>
