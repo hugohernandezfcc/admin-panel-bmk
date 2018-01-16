abe3dc81c4ff44224d0a753264dfc721c79091c0
Antonio Gervacio <luis.gervaciot@gmail.com>
2018-01-15 18:17:47 -0600

    Test 1817

--- resources/views/menus.blade.php
+++ resources/views/menus.blade.php
@@ -7,78 +7,50 @@
 @stop
 
 @section('content')
-    <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Menus
-        <small>menus database</small>
-      </h1>
-    </section>
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="row">
-        <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header">
-              <h3 class="box-title">menus database</h3>
-            </div>
-
-            <!-- /.box-header -->
-            <div class="box-body">
-              <table id="example1" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                  <th>Text</th>
-                  <th>Order</th>
-                  <th>Label</th>
-                  <th>Icon </th>
-                  <th>Label Color</th>
-                  <th>URL</th>
-                  <th>To</th>
-                  <th>Type Item</th>
-                  <th>Parent</th>
-                </tr>
-                </thead>
-                <tbody>
-                    @foreach ($menus as $key => $menu)
-                        <tr>
-                            <td>{{ $menu -> text}}</td>
-                            <td>{{ $menu -> order}}</td>
-                            <td>{{ $menu -> label}}</td>
-                            <td>{{ $menu -> icon}}</td>
-                            <td>{{ $menu -> label_color}}</td>
-                            <td>{{ $menu -> url}}</td>
-                            <td>{{ $menu -> to}}</td>
-                            <td>{{ $menu -> typeitem}}</td>
-                            <td>{{ $menu -> parent}}</td>
-                        </tr>
-                    @endforeach
-                <tfoot>
-               <tr>
-                  <th>Text</th>
-                  <th>Order</th>
-                  <th>Label</th>
-                  <th>Icon</th>
-                  <th>Label Color</th>
-                  <th>URL</th>
-                  <th>To</th>
-                  <th>Type Item</th>
-                  <th>Parent</th>
-                </tr>
-                </tfoot>
-              </table>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
-@stop
+<!-- /.box-header -->
+<div class="box-body">
+  <table id="example1" class="table table-bordered table-striped">
+    <thead>
+    <tr>
+      <th>Text</th>
+      <th>Order</th>
+      <th>Label</th>
+      <th>Icon </th>
+      <th>Label Color</th>
+      <th>URL</th>
+      <th>To</th>
+      <th>Type Item</th>
+      <th>Parent</th>
+    </tr>
+    </thead>
+    <tbody>
+        @foreach ($menus as $key => $menu)
+            <tr>
+                <td>{{ $menu -> text}}</td>
+                <td>{{ $menu -> order}}</td>
+                <td>{{ $menu -> label}}</td>
+                <td>{{ $menu -> icon}}</td>
+                <td>{{ $menu -> label_color}}</td>
+                <td>{{ $menu -> url}}</td>
+                <td>{{ $menu -> to}}</td>
+                <td>{{ $menu -> typeitem}}</td>
+                <td>{{ $menu -> parent}}</td>
+            </tr>
+        @endforeach
+    <tfoot>
+   <tr>
+      <th>Text</th>
+      <th>Order</th>
+      <th>Label</th>
+      <th>Icon</th>
+      <th>Label Color</th>
+      <th>URL</th>
+      <th>To</th>
+      <th>Type Item</th>
+      <th>Parent</th>
+    </tr>
+    </tfoot>
+  </table>
+</div>
+<!-- /.box-body -->
