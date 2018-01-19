eca7a909a964fe45f551b64a588d87301e2e7745
Antonio Gervacio <luis.gervaciot@gmail.com>
2018-01-19 13:20:52 -0600

    Test DB - 1320

--- routes/web.php
+++ routes/web.php
@@ -58,12 +58,12 @@ Route::get('/pruebaDB', function () {
 		    echo $title;
 		}
 	*/
-		/*
+		
 		$tables = DB::select("SELECT table_schema,table_name, table_catalog FROM information_schema.tables WHERE table_catalog = 'DB_DATABASE' AND table_type = 'BASE TABLE' AND table_schema = 'public' ORDER BY table_name;")
 		foreach($tables as $table)
 		{
 		      echo $table->privacy_statements;
 		}
-		*/
+		
 
 });
