6eac96297d299dbd3c9343505cbe0c1c44b649c7
Antonio Gervacio <luis.gervaciot@gmail.com>
2018-01-23 11:24:40 -0600

    Test 1124

--- resources/views/campos.blade.php
+++ resources/views/campos.blade.php
@@ -9,11 +9,34 @@
 @section('content')
     <h1>{{$variable1}}</h1>
     @foreach ($tablas as $tabla)
-        <h2>{{$tabla}}</h2>
-        @foreach ($campos as $campo)
-            @if($campo['table']==$tabla)
-              <h2>{{$campo['field']}}</h2>
-            @endif
-        @endforeach
+        <!-- /.box-header -->
+        <div class="box-body">
+          <table id="example" class="table table-bordered table-striped" title="Campos de la tabla $tabla">
+            <thead>
+            <tr>
+              <th>Field</th>
+            </tr>
+            </thead>
+            <tbody>
+                @foreach ($campos as $campo)
+                    @if($campo['table']==$tabla)
+                        <tr>
+                            <td>{{ $campo['field']}}</td>
+                        </tr>
+                    @endif
+                @endforeach
+            </tbody>
+            <tfoot>
+            <tr>
+              <th>School of Medicine</th>
+              <th>Faculty of Specialization</th>
+              <th>Practise Professional</th>
+              <th>User</th>
+              <th>Professional License</th>
+            </tr>
+            </tfoot>
+          </table>
+        </div>
+        <!-- /.box-body -->
     @endforeach
 @stop
