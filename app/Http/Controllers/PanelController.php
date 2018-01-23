c8e81fe0854756db489d2d10ee9b120f3246f01b
Antonio Gervacio <luis.gervaciot@gmail.com>
2018-01-22 18:56:25 -0600

    Test 1856

--- app/Http/Controllers/PanelController.php
+++ app/Http/Controllers/PanelController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Database\Schema;
+use Illuminate\Database\Schema\Builder;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\menu;
