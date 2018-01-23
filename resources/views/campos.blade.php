f11a016943cbc71de3a7d58e846fe18b8949479c
Antonio Gervacio <luis.gervaciot@gmail.com>
2018-01-22 18:16:41 -0600

    Test 1816

--- resources/views/campos.blade.php
+++ resources/views/campos.blade.php
@@ -9,24 +9,6 @@
 @section('content')
             
             @foreach ($tables as $table)
-            <!-- /.box-header -->
-            <div class="box-body">
-              <table id="example" class="table table-bordered table-striped">
-                <thead>
-                <tr>
-                  <th>Field</th>
-                </tr>
-                </thead>
-                <tbody>
-                   
-                </tbody>
-                <tfoot>
-                <tr>
-                  <th>Field</th>
-                </tr>
-                </tfoot>
-              </table>
-            </div>
-            <!-- /.box-body -->
+                <h1>{{table}}</h1>>
             @endforeach
 @stop
