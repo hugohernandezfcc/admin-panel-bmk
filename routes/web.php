7e71667c408e6a14d7d61bb874e91ee529245df7
Antonio Gervacio <luis.gervaciot@gmail.com>
2018-01-23 13:53:50 -0600

    Test 1353

--- routes/web.php
+++ routes/web.php
@@ -133,7 +133,7 @@ Route::get('/pruebaDB', function () {
         */
         $query = 'select column_name, data_type from information_schema.columns where table_schema = \'public\' and table_name = \'menus\';';
         echo $query;
-        $menus = DB::select();
+        $menus = DB::select($query);
 
         foreach ($menus as $menu)
         {
